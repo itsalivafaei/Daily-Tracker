6baa139130dd936227f6d83162f6a7b21961611b
alivafaei <alivafaei.alive@gmail.com>
2023-01-05 02:40:25 +0330

    0: some changes in GoogleServices.kt based on Codelab

--- app/src/main/java/com/example/dailytracker/googleServices/GoogleServices.kt
+++ app/src/main/java/com/example/dailytracker/googleServices/GoogleServices.kt
@@ -1,10 +1,14 @@
 package com.example.dailytracker.googleServices
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.view.View
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailabilityLight
 
-class GoogleServices(private val mainActivityInstance: Context) {
+class GoogleServices(private val mainActivityContext: Context) {
     private val googlePlayServicesAvailability: (Context, Int) -> Boolean = { mainInstance, minSdkVersion ->
         val googleApiAvailabilityLight: GoogleApiAvailabilityLight =
             GoogleApiAvailabilityLight.getInstance()
@@ -12,9 +16,22 @@ class GoogleServices(private val mainActivityInstance: Context) {
         status == ConnectionResult.SUCCESS
 
     }
+    //Sleep Permission check
+    private val activityRecognitionPermissionApproved: () -> Boolean = {
+        PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+            mainActivityContext,
+            Manifest.permission.ACTIVITY_RECOGNITION
+        )
+    }
 
 
     fun googlePlayServiceAvailabilityCheck(): Boolean {
-        return googlePlayServicesAvailability(mainActivityInstance, 29)
+        //activity recognition works on minimum SDK 29
+        return googlePlayServicesAvailability(mainActivityContext, 29)
+    }
+
+    fun onClickRequestSleepData(view: View) {
+        if (activityRecognitionPermissionApproved()) {
+        }
     }
 }
